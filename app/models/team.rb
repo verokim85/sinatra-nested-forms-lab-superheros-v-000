eec5e3733ea26ce2c0160b3b4671361b6bc897c1
Veronica Kim <vero.a.kim@gmail.com>
2018-05-26 19:11:39 +0000

    automatically backed up by learn

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,27 +1,11 @@
 class Team
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
   attr_accessor :name, :motto
 
   @@teams = []
-=======
->>>>>>> 6282057d68d2a27425dc184d36acc889494172da
-
-  TEAMS = []
-
-  attr_accessor :name, :motto
-<<<<<<< HEAD
-=======
->>>>>>> 590fb3c9e50bd861cf1e6c7e8c740f70637af6a9
->>>>>>> 6282057d68d2a27425dc184d36acc889494172da
 
   def initialize(params)
     @name = params[:name]
     @motto = params[:motto]
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
     @@teams << self
   end
 
@@ -34,17 +18,3 @@ class Team
   end
 
 end
-=======
->>>>>>> 6282057d68d2a27425dc184d36acc889494172da
-    TEAMS << self
-  end
-
-  def self.all
-    TEAMS
-  end
-
-end
-<<<<<<< HEAD
-=======
->>>>>>> 590fb3c9e50bd861cf1e6c7e8c740f70637af6a9
->>>>>>> 6282057d68d2a27425dc184d36acc889494172da
