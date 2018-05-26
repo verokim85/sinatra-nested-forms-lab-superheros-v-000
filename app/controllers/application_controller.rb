c6fa0b4612c4980cf983df6585c2d3284f2fce64
Veronica Kim <vero.a.kim@gmail.com>
2018-05-26 19:11:51 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,22 +9,13 @@ class App < Sinatra::Base
     end
 
     post '/teams' do
-
+      # binding.pry
       @team = Team.new(params[:team])
 
       params[:team][:hero].each do |details|
-<<<<<<< HEAD
-=======
-
         Superhero.new(details)
       end
       @heroes = Superhero.all
-
->>>>>>> 6282057d68d2a27425dc184d36acc889494172da
-        Hero.new(details)
-      end
-
-      @heroes = Hero.all
 
       erb :team
     end
